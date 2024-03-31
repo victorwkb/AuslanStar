53177e23b62b021fd9b7cb1a9dad937b8eddd0ff
Mingxin Hu <humingxin0512@gmai.com>
2024-04-01 03:36:43 +1100

    Navigation Bar + Footer Version 1

---
+++ webapp/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import React from "react";
+import Footer from "@/components/Footer";
+
+
+export const metadata: Metadata = {
+  title: "AuslanStar",
+  description: "AuslanStar is a platform for learning Australian Sign Language (Auslan) online.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+
+            {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ webapp/constants/index.ts
@@ -0,0 +1,58 @@
+
+
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'dictionary', label: 'Dictionary' },
+    { href: '/', key: 'resources', label: 'Resources' },
+    // ... more items
+];
+// SUB-NAVIGATION
+export const RESOURCE_LINKS = [
+  { href: '/benefits', key: 'benefits', label: 'Benefits of Learning Auslan' },
+  { href: '/support', key: 'support', label: 'Supporting Your Child' },
+  { href: '/simulator', key: 'simulator', label: 'Hearing Loss Simulator' },
+  // ... more items
+];
+
+
+
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+    {
+        title: 'Learn More',
+        links: [
+            'About Hilink',
+            'Press Releases',
+            'Environment',
+            'Jobs',
+            'Privacy Policy',
+            'Contact Us',
+        ],
+    },
+    {
+        title: 'Our Community',
+        links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    },
+];
+
+export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+        { label: 'Admin Officer', value: '123-456-7890' },
+        { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    ],
+};
+
+export const SOCIALS = {
+    title: 'Social',
+    links: [
+        '/facebook.svg',
+        '/instagram.svg',
+        '/twitter.svg',
+        '/youtube.svg',
+        '/wordpress.svg',
+    ],
+};
