6528a01ae766e49be01aeb9f7dfbf04812c2c944
Mingxin Hu <humingxin0512@gmai.com>
2024-04-03 04:33:11 +1100

    sub-page update

--- webapp/app/resources/support/page.tsx
+++ webapp/app/resources/support/page.tsx
@@ -3,18 +3,13 @@ import Link from "next/link"
 
 
 export default function Resources() {
-  return (
-    <div className="flex flex-col gap-16">
-      <div>
-        <h1 className="animate-in text-3xl font-bold tracking-tight">Blog</h1>
-
-          <Link href="/">Link to Home Page</Link>
-
-      </div>
-      <p className="animate-in text-secondary" >
-        Support
-          Coming soon...
-      </p>
-    </div>
-  );
+    return (
+        <div className="max-container padding-container">
+            <div className="flex flex-col gap-16">
+                <p className="animate-in text-secondary">
+                    Supporting Your Child
+                </p>
+            </div>
+        </div>
+    );
 }
