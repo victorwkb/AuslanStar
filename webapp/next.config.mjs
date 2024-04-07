17304fab255586579d88d5fdf7209c31c6e6f837
victorwkb <victorwkb@gmail.com>
2024-04-08 02:03:48 +1000

    upload vectors via langchain, pgvector to supabase and upload dictionary to mongodb

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    BEDROCK_AWS_REGION: process.env.BEDROCK_AWS_REGION,
+    BEDROCK_AWS_ACCESS_KEY_ID: process.env.BEDROCK_AWS_ACCESS_KEY_ID,
+    BEDROCK_AWS_SECRET_ACCESS_KEY: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY,
   }
 };
 
