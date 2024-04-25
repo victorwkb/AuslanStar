76f29e372bffe54528d182cfbaca3c49ce0f35f1
victorwkb <victorwkb@gmail.com>
2024-04-25 18:55:43 +1000

    feat: fetch sagemaker api

--- webapp/app/learn/page.tsx
+++ webapp/app/learn/page.tsx
@@ -15,32 +15,63 @@ const words: { letter: string, sign: string }[] = [
 ]
 
 
+
 export default function Learn() {
-  const webcamRef = useState<Webcam>
-  const canvasRef = useRef<HTMLCanvasElement>;
+  const webcamRef = useRef<Webcam>();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const [video, setVideo] = useState<boolean>(false);
   const [mirrored, setMirrored] = useState<boolean>(true);
   const [sign, setSign] = useState<string>("A");
+  const [countdown, setCountdown] = useState<number>(3);
 
-  const path = `/game/${sign}`;
+  const startCountdown = () => {
+    setCountdown(3);
+    const countdownInterval = setInterval(() => {
+      setCountdown((prevCount) => prevCount - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(countdownInterval);
+      captureImage();
+    }, 3000);
+  };
+
+  const captureImage = async () => {
+    if (!webcamRef.current) {
+      console.log('Webcam not found');
+      return;
+    } else {
+      const imageSrc = webcamRef.current.getScreenshot();
+      if (!imageSrc) {
+        console.log('Image not found');
+        return;
+      } else {
+        await sendImageToAPI(imageSrc);
+      }
+    }
+  };
+
+  const sendImageToAPI = async (image: string) => {
+    const response = await fetch('https://f5q7vzkp2l.execute-api.ap-southeast-2.amazonaws.com/invoke_sage_maker', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ image }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+
 
   return (
-    {/*
     <div className="max-container py-12 bg-white">
       <div className="flex flex-row gap-x-12 items-center justify-center min-h-[600px]">
         <div className="flex flex-col items-center justify-center border border-gray-200 shadow-lg rounded-lg w-[600px] h-[600px] bg-gray-100">
-          <div className="w-full h-full items-center justify-center">
-            {sign &&
-              <Image
-                src={words.find((word) => word.letter === sign).sign}
-                alt={sign}
-                width={600}
-                height={600}
-                className="object-contain p-2"
-              />
-            }
-          </div>
 
           <div className='flex flex-row gap-x-3 h-12 mb-6'>
             <Button>
@@ -65,23 +96,28 @@ export default function Learn() {
             {video ? (
               <Webcam
                 open={video}
+                audio={false}
                 mirrored={mirrored}
                 className="h-full w-full object-contain p-2"
+                screenshotFormat="image/jpeg"
+                ref={webcamRef}
               />
             ) : (
               <canvas ref={canvasRef} className="h-full w-full object-contain p-2" />
             )
             }
           </div>
+
           <div className="flex flex-row items-center justify-center h-12 mb-6">
-            <Button>
+            <Button onClick={startCountdown} >
               <CameraIcon className="w-[50px] h-[50px]" />
             </Button>
 
             <Button
               onClick={() => {
                 setVideo((video) => !video)
-              }}>
+              }}
+            >
               {video ? (
                 <VideoIcon className="w-[50px] h-[50px]" />)
                 : (
@@ -89,6 +125,9 @@ export default function Learn() {
               }
             </Button>
 
+            <div>
+              {countdown > 0 && <p>Countdown: {countdown}</p>}
+            </div>
           </div>
         </div>
       </div>
@@ -96,3 +135,14 @@ export default function Learn() {
   )
 }
 
+// <div className="w-full h-full items-center justify-center">
+//   {sign && words.find((word) => word.letter === sign) &&
+//     <Image
+//       src={words.find((word) => word.letter === sign).sign}
+//       alt={sign}
+//       width={600}
+//       height={600}
+//       className="object-contain p-2"
+//     />
+//   }
+// </div>
