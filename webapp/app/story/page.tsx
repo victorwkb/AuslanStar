784e46f23a8552df410190db272afde713db4926
aranqiao <aranqiao42@gmail.com>
2024-05-14 05:26:55 +1000

    story pages added

--- webapp/app/story/page.tsx
+++ webapp/app/story/page.tsx
@@ -1,56 +1,56 @@
 "use client";
 
-import React, { useState } from 'react';
-import YouTube from 'react-youtube';
+// app/story/page.tsx
 
-// YouTube video id list
-const episodes = [
-  { id: 'xu6JMJTEaXc', title: 'Episode 1' }, 
-  { id: '84nURKHLDdY', title: 'Episode 2' },
-  { id: 'FuO0oxGT55I', title: 'Episode 3' },
-  { id: 'QDLr5SIUTMU', title: 'Episode 4' }, 
-  { id: 'nocJRFFl60c', title: 'Episode 5' },
-  { id: 'apbqKLID9ig', title: 'Episode 6' }, 
-  { id: 't-3-ekRdt7w', title: 'Episode 7' },
-  { id: 'ZSfZAGwHTw', title: 'Episode 8' },
-  { id: 'ueYN1vZ1xjc', title: 'Episode 9' },
-  { id: 'M_3HdU8uhZs', title: 'Episode 10' },
-  { id: 'af77Zz1yfhw', title: 'Episode 11' }, 
-  { id: 'w5d8I0Pj3X4', title: 'Episode 12' },
-  { id: 'qVOx3mpx6SA', title: 'Episode 13' },
-  { id: 'bJ9mISWgL4s', title: 'Episode 14' }, 
-  { id: '2uW_fCgaNH4', title: 'Episode 15' },
-];
+import Link from 'next/link';
 
-
-
-export default function VideoPlayer() {
-  const [currentVideoId, setCurrentVideoId] = useState(episodes[0].id);   // The first video is played by default
-
-  const videoOptions = {
-    height: '600',
-    width: '1000',
-    playerVars: {
-      autoplay: 1,  // 1: autoplay the video
-      // Change the link based on the actual deployment address
-      origin: 'http://localhost:3000'  
-    },
-  };
+export default function SeasonsOverview() {
+  const seasons = [
+    { id: 1, title: 'Season 1', description: 'Sally and Possum explore learning opportunities and adventures in everyday life through a variety of interactive and creative activities.', image: '/story/season1.png' },
+    { id: 2, title: 'Season 2', description: 'Sally and Possum explore science and math through daily activities.', image: '/story/season2.png' },
+    { id: 3, title: 'Season 3', description: 'Sally and Possum engage in a variety of educational adventures, learning about nature, animals, crafts, and everyday objects through interactive and playful experiences.', image: '/story/season3.png' },
+    { id: 4, title: 'Season 4', description: 'Sally and Possum explore practical skills, safety, nature, and creativity through various engaging activities and experiments.', image: '/story/season4.png' },
+    { id: 5, title: 'Season 5', description: 'Sally and Possum learn about tools, reflections, motion, and problem-solving through various fun and interactive adventures.', image: '/story/season5.png' },
+    { id: 6, title: 'Season 6', description: 'Sally and Possum learn about science, practical skills, and sustainability through fun lessons with Sally and Skip.', image: '/story/season6.png' },
+  ];
 
   return (
-    <div className="flex">
-      <div className="w-64 bg-gray-800 text-white p-5">
-        <h2 className="text-xl font-bold mb-4">On this page</h2>
-        <ul>
-          {episodes.map((episode, index) => (
-            <li key={index} className={`cursor-pointer p-2 hover:bg-gray-700 ${currentVideoId === episode.id ? 'bg-gray-600' : ''}`} onClick={() => setCurrentVideoId(episode.id)}>
-              {episode.title}
-            </li>
-          ))}
-        </ul>
+    <div>
+      {/* Header */}
+      <div className="bg-orange-300">
+        <div className="animate-in max-w-5xl px-4 py-10 max-container padding-container">
+          <h1 className="text-5xl font-bold text-white leading-tight whitespace-normal">
+            StoryTelling in Auslan
+          </h1>
+          <p className="text-2xl text-white mt-2">
+            An innovative and beautiful video series for young children who use Auslan as their primary means of communication
+          </p>
+        </div>
+        <svg width="100%" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"
+          preserveAspectRatio="none" style={{ display: 'block' }}>
+          {/* Modify the fill color to match your bg-green-100; use the correct color code */}
+          <path fill="#FEFCE8"
+            d="M0,0 L48,14.7 C96,29,192,59,288,74.7 C384,91,480,101,576,85.3 C672,69,768,27,864,16 C960,5,1056,35,1152,48 C1248,61,1344,59,1392,57.3 L1440,56 L1440,150 L1392,150 C1344,150,1248,150,1152,150 C1056,150,960,150,864,150 C768,150,672,150,576,150 C480,150,384,150,288,150 C192,150,96,150,48,150 L0,150 Z"></path>
+        </svg>
       </div>
-      <div className="flex-grow p-5">
-        <YouTube videoId={currentVideoId} opts={videoOptions} />
+
+      <div className="container mx-auto px-4 py-10">
+        <h2 className="text-4xl font-bold text-center mb-14">Choose a Season now!</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {seasons.map((season) => (
+            <Link key={season.id} href={`/story/season${season.id}`} passHref>
+              <div className="block p-4 rounded-lg shadow-lg bg-orange-100 hover:bg-orange-200 hover:scale-125 hover:font-bold">
+                <div className="img-container">
+                  <img src={season.image} alt={`Cover of ${season.title}`} className="w-full object-cover rounded" style={{ height: '200px' }} />
+                </div>
+                <div className="mt-4 text-container">
+                  <h3 className="text-xl font-semibold">{season.title}</h3>
+                  <p className="text-gray-600">{season.description}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ webapp/app/story/season3/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useState } from 'react';
+import YouTube from 'react-youtube';
+import Link from 'next/link';
+
+// YouTube video id list
+const episodes = [
+  { id: 'ohG-fhzLlgA', title: 'E1 Possum wants to go to school' },
+  { id: 'wqH0ZjYpJNo', title: 'E2 Bush walk' },
+  { id: 'qR7Undu3jLc', title: 'E3 What is wool' },
+  { id: 'Jf5fxWvg3Ng', title: 'E4 Kangaroos' },
+  { id: 'TyxzjbljQKs', title: 'E5 Possum\'s classroom' },
+  { id: 'CZrh_9mBEbI', title: 'E6 Dinner guest' },
+  { id: '7htU6zwRfAo', title: 'E7 Where do eggs come from' },
+  { id: 'id7My8UxvNE', title: 'E8 Possum breaks a plate' },
+  { id: 'ddklM_kocEY', title: 'E9 Possum wants to be taller' },
+  { id: 'wFWq47ENZaU', title: 'E10 Smallest carrots' },
+  { id: '9xv5sxbve1I', title: 'E11 The football' },
+  { id: 'RfwPfRNrKPM', title: 'E12 Possum chooses breakfast' },
+  { id: 'IBq8wDjJX38', title: 'E13 King of the castle' },
+  { id: 'BExZGy9m70U', title: 'E14 Possum in charge' },
+  { id: 'Hxzml7EKw_U', title: 'E15 Possum joins the circus' },
+];
+
+
+export default function VideoPlayer() {
+  const [currentVideoId, setCurrentVideoId] = useState(episodes[0].id);
+
+  const videoOptions = {
+    height: '1000',
+    width: '1200',
+    playerVars: {
+      autoplay: 1,
+      origin: 'http://localhost:3000'
+    },
+  };
+
+  return (
+    <div>
+      {/* Header */}
+      <div className="bg-orange-300">
+        <div className="animate-in max-w-5xl px-4 py-10 max-container padding-container">
+          <h1 className="text-5xl font-bold text-white leading-tight whitespace-normal">
+            Sally and Possum - Season 3
+          </h1>
+        </div>
+        <svg width="100%" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"
+          preserveAspectRatio="none" style={{ display: 'block' }}>
+          {/* Modify the fill color to match your bg-green-100; use the correct color code */}
+          <path fill="#FEFCE8"
+            d="M0,0 L48,14.7 C96,29,192,59,288,74.7 C384,91,480,101,576,85.3 C672,69,768,27,864,16 C960,5,1056,35,1152,48 C1248,61,1344,59,1392,57.3 L1440,56 L1440,150 L1392,150 C1344,150,1248,150,1152,150 C1056,150,960,150,864,150 C768,150,672,150,576,150 C480,150,384,150,288,150 C192,150,96,150,48,150 L0,150 Z"></path>
+        </svg>
+      </div>
+
+      <div className="flex min-h-screen bg-gradient-to-br">
+        
+        <div className="w-1/4 bg-white p-5 shadow-lg">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">Episodes</h2>
+          <ul className="space-y-2">
+            {episodes.map((episode, index) => (
+              <li key={index}
+                className={`cursor-pointer p-3 rounded-lg hover:bg-blue-500 hover:text-white ${currentVideoId === episode.id ? 'bg-blue-600 text-white' : 'text-gray-700'}`}
+                onClick={() => setCurrentVideoId(episode.id)}>
+                {episode.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+
+        <div className="flex-grow p-10">
+          <YouTube videoId={currentVideoId} opts={videoOptions} />
+        </div>
+      </div>
+
+
+      <div className="max-w-5xl mx-auto mt-4 flex justify-between space-x-10 px-4">
+        <Link href="/story/season2">
+          <p className="flex items-center space-x-2 cursor-pointer hover:scale-125 active:scale-100 hover:font-bold transition-all duration-100">
+            <img src="/game/left.png" alt="Backward" className="w-20 h-20" />
+            <span className="text-lg">Back to Season 2</span>
+          </p>
+        </Link>
+
+        <Link href="/story/season4">
+          <p className="flex items-center space-x-2 cursor-pointer hover:scale-125 active:scale-100 hover:font-bold transition-all duration-100">
+            <img src="/game/right.png" alt="Forward" className="w-20 h-20" />
+            <span className="text-lg">Go to Season 4</span>
+          </p>
+        </Link>
+      </div>
+
+      <svg width="100%" height="20%" className="fill-current bg-yellow-50 text-indigo-900 pt-8" viewBox="0 80 1440 70" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 0,400 L 0,150 C 84.29999999999998,140.8948717948718 168.59999999999997,131.78974358974358 248,125 C 327.40000000000003,118.2102564102564 401.9,113.73589743589744 475,123 C 548.1,132.26410256410256 619.8,155.26666666666668 714,146 C 808.2,136.73333333333332 924.9000000000001,95.19743589743588 1001,87 C 1077.1,78.80256410256412 1112.6,103.94358974358974 1179,120 C 1245.4,136.05641025641026 1342.7,143.02820512820512 1440,150 L 1440,400 L 0,400 Z"></path>
+      </svg>
+
+
+    </div>
+  );
+}
